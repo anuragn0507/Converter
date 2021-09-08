49538aaaf1f20e40606425d3654e0255acf56020
Anurag <anuragn1107@gmail.com>
2021-09-09 03:12:51 +0530

    Kg Converter App

--- app/src/main/java/com/anurag/converter/MainActivity.java
+++ app/src/main/java/com/anurag/converter/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText("The corresponding valuea in Pounds is  " + pound);
 
             }
-        });
+          });
 
     }
 }
